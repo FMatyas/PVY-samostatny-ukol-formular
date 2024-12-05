2d1b2ca500b745b04d014f0de7092335dcc7ebd4
já už nevim <149662501+FMatyas@users.noreply.github.com>
2024-12-05 11:14:09 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,33 +1,32 @@
 function toggleForm() {
     const selectedValue = document.getElementById("form-select").value;
-    document.getElementById("obvod-form").style.display = selectedValue === "obvod" ? "block" : "none";
     document.getElementById("obsah-form").style.display = selectedValue === "obsah" ? "block" : "none";
+    document.getElementById("obvod-form").style.display = selectedValue === "obvod" ? "block" : "none";
 }
-
-const obvodStrA = document.getElementById('strana-a');
-const obvodStrB = document.getElementById('strana-b');
-const obvodStrC = document.getElementById('strana-c');
-const btnObvod = document.getElementById('btnObvod');
-const outputObvod = document.getElementById('vysledekObvod');
-
-btnObvod.addEventListener('click', function () {
-    event.preventDefault();
-    const a = parseFloat(obvodStrA.value) || 0;
-    const b = parseFloat(obvodStrB.value) || 0;
-    const c = parseFloat(obvodStrC.value) || 0;
-    const vystup = a + b + c;
-    outputObvod.innerHTML = vystup ? `${vystup} cm` : "Chyba: Zadejte platné hodnoty.";
-});
-
 const obsahZakladna = document.getElementById('zakladna');
-const obsahVyska = document.getElementById('vyska');
+const obsahDruheStrany = document.getElementById('druhaStrana');
 const btnObsah = document.getElementById('btnObsah');
 const outputObsah = document.getElementById('vysledekObsah');
 
 btnObsah.addEventListener('click', function () {
     event.preventDefault();
     const zakladna = parseFloat(obsahZakladna.value) || 0;
-    const vyska = parseFloat(obsahVyska.value) || 0;
-    const vystup = (zakladna * vyska) / 2;
-    outputObsah.innerHTML = vystup ? `${vystup} cm²` : "Chyba: Zadejte platné hodnoty.";
+    const druhaStrana = parseFloat(obsahDruheStrany.value) || 0;
+    const vystupObsahu = (zakladna * druhaStrana) / 2;
+    outputObsah.innerHTML = vystupObsahu ? `${vystupObsahu} cm²` : "Chyba: Zadejte platné hodnoty.";
 })
+
+const obvodA = document.getElementById('strana-a');
+const obvodB = document.getElementById('strana-b');
+const obvodC = document.getElementById('strana-c');
+const btnObvod = document.getElementById('btnObvod');
+const outputObvod = document.getElementById('vysledekObvod');
+
+btnObvod.addEventListener('click', function () {
+    event.preventDefault();
+    const a = parseFloat(obvodA.value) || 0;
+    const b = parseFloat(obvodB.value) || 0;
+    const c = parseFloat(obvodC.value) || 0;
+    const vystupObvodu = a + b + c;
+    outputObvod.innerHTML = vystupObvodu ? `${vystupObvodu} cm` : "Chyba: Zadejte platné hodnoty.";
+});
